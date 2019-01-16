fbee24e08dc9509006e72aabe2bad970c4a93078
lukjack <lukjack92@gmail.com>
2019-01-16 21:36:58 +0100

    Update StopDowntime.sh

--- StopDowntime.sh
+++ StopDowntime.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Stop downtime"
 
-mv /var/www/html/downtime/index.html /var/www/html/downtime/index.html.save 2>/$
+mv /var/www/html/downtime/index.html /var/www/html/downtime/index.html.save 2>/dev/null
 if [[ $? == 0 ]]; then
   echo "StartDowntime is run - downtime is disable"
 else
