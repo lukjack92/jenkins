f01865912adf96773b4fdb8c55a8c6375bd831f0
lukjack <lukjack92@gmail.com>
2019-01-16 21:21:38 +0100

    Update StartDowntime.sh

--- StartDowntime.sh
+++ StartDowntime.sh
@@ -3,7 +3,7 @@ echo "Start downtime"
 
 mv /var/www/html/downtime/index.html.save /var/www/html/downtime/index.html 2>/$
 if [[ $? == 0 ]]; then
-  echo "StartDowntime is run - downtime is running"
+  echo "StartDowntime is run - downtime is enable"
 else
   echo "StartDowntime has been run"
 fi
