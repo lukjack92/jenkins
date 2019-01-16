2ff7c33f2b71809e8d5d07e67ff98473a5dbaa88
lukjack <lukjack92@gmail.com>
2019-01-16 21:20:10 +0100

    Update and rename test.sh to StartDowntime.sh

---
+++ StartDowntime.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Start downtime"
+
+mv /var/www/html/downtime/index.html.save /var/www/html/downtime/index.html 2>/$
+if [[ $? == 0 ]]; then
+  echo "StartDowntime is run - downtime is running"
+else
+  echo "StartDowntime has been run"
+fi
--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Test from Jenkins"
-date >> test.log
-pwd >> test.log 
-ls >> test.log
-echo "Koniec1"
-echo "Koniec2"
